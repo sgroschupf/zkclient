a0f87519506fa65b05a3c8db08bfdab9e858d18d
Peter Voss <info@petervoss.org>
2009-07-15 08:56:37 +0200

    Added implementation for a distributed queue.

---
+++ src/main/java/org/I0Itec/zkclient/DistributedQueue.java
@@ -0,0 +1,62 @@
+package org.I0Itec.zkclient;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Queue;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.NoNodeException;
+
+public class DistributedQueue<T extends Serializable> {
+
+    private ZkClient _zkClient;
+    private String _root;
+
+    private static final String ELEMENT_NAME = "element";
+
+    public DistributedQueue(ZkClient zkClient, String root) {
+        _zkClient = zkClient;
+        _root = root;
+    }
+
+    public boolean offer(T element) throws KeeperException, InterruptedException, IOException {
+        _zkClient.create(_root + "/" + ELEMENT_NAME + "-", element, CreateMode.PERSISTENT_SEQUENTIAL);
+        return true;
+    }
+
+    public T poll() throws KeeperException, InterruptedException, IOException {
+        Queue<String> q;
+        while (true) {
+            List<String> list = _zkClient.getChildren(_root, true);
+            if (list.size() == 0) {
+                return null;
+            }
+            String elementName = getSmallestElement(list);
+
+            try {
+                T data = (T) _zkClient.readData(_root + "/" + elementName);
+                _zkClient.delete(_root + "/" + elementName);
+                return data;
+            } catch (NoNodeException e) {
+                // somebody picked up the element first
+            }
+        }
+    }
+
+    private String getSmallestElement(List<String> list) {
+        String smallestElement = list.get(0);
+        for (String element : list) {
+            if (element.compareTo(smallestElement) < 0) {
+                smallestElement = element;
+            }
+        }
+        
+        return smallestElement;
+    }
+
+    public boolean isEmpty() throws KeeperException, InterruptedException {
+        return _zkClient.getChildren(_root, false).size() == 0;
+    }
+}
---
+++ src/test/java/org/I0Itec/zkclient/DistributedQueueTest.java
@@ -0,0 +1,89 @@
+package org.I0Itec.zkclient;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
+
+import org.apache.zookeeper.KeeperException;
+import org.junit.Test;
+
+public class DistributedQueueTest {
+
+    @Test(timeout = 15000)
+    public void testDistributedQueue() throws InterruptedException, IOException, KeeperException {
+        ZkServer zkServer = ZkTestUtil.startZkServer("ZkClientTest-testDistributedQueue", 4711);
+        ZkClient client = new ZkClient("localhost:4711", 5000);
+        client.createPersistent("/queue");
+
+        DistributedQueue<Long> distributedQueue = new DistributedQueue<Long>(client, "/queue");
+        distributedQueue.offer(17L);
+        distributedQueue.offer(18L);
+        distributedQueue.offer(19L);
+
+        assertEquals(Long.valueOf(17L), distributedQueue.poll());
+        assertEquals(Long.valueOf(18L), distributedQueue.poll());
+        assertEquals(Long.valueOf(19L), distributedQueue.poll());
+
+        client.close();
+        zkServer.shutdown();
+        zkServer.join();
+    }
+
+    @Test(timeout = 15000)
+    public void testMultipleReadingThreads() throws InterruptedException, IOException, KeeperException {
+        ZkServer zkServer = ZkTestUtil.startZkServer("ZkClientTest-testDistributedQueue", 4711);
+        ZkClient client = new ZkClient("localhost:4711", 5000);
+        client.createPersistent("/queue");
+
+        final DistributedQueue<Long> distributedQueue = new DistributedQueue<Long>(client, "/queue");
+
+        // insert 1000 elements
+        for (int i = 0; i < 100; i++) {
+            distributedQueue.offer(new Long(i));
+        }
+
+        // 5 reading threads
+        final Set<Long> readElements = Collections.synchronizedSet(new HashSet<Long>());
+        List<Thread> threads = new ArrayList<Thread>();
+        final List<Exception> exceptions = new Vector<Exception>();
+
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            Long value = distributedQueue.poll();
+                            if (value == null) {
+                                return;
+                            }
+                            readElements.add(value);
+                        }
+                    } catch (Exception e) {
+                        exceptions.add(e);
+                        e.printStackTrace();
+                    }
+                }
+            };
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        
+        assertEquals(0, exceptions.size());
+        assertEquals(100, readElements.size());
+
+        client.close();
+        zkServer.shutdown();
+        zkServer.join();
+    }
+}
